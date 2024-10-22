7ff838552f1c5ab5c64206c00aa0910990312645
wandering_4 <17891997260@139.com>
2024-10-22 22:45:13 +0800

    图片上传功能

---
+++ LittleRedBook-oss/LittleRedBook-oss-biz/src/main/java/com/haishi/LittleRedBook/oss/biz/strategy/FileStrategy.java
@@ -0,0 +1,14 @@
+package com.haishi.LittleRedBook.oss.biz.strategy;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStrategy {
+    /**
+     * 文件上传
+     *
+     * @param file
+     * @param bucketName
+     * @return
+     */
+    String uploadFile(MultipartFile file, String bucketName);
+}
