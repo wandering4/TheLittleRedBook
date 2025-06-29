3d86f1c1d68c29492b92958aa90dcc52fbda14a0
wandering_4 <17891997260@139.com>
2025-06-29 20:18:29 +0800

    fix

--- LittleRedBook-note/LittleRedBook-note-biz/src/test/java/com/haishi/LittleRedBook/biz/DruidTest.java
+++ LittleRedBook-note/LittleRedBook-note-biz/src/test/java/com/haishi/LittleRedBook/biz/DruidTest.java
@@ -1,7 +1,6 @@
 package com.haishi.LittleRedBook.biz;
 
 import com.haishi.LittleRedBook.note.biz.LittleRedBookNoteBizApplication;
-import com.haishi.LittleRedBook.note.biz.service.ChannelService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -9,12 +8,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = LittleRedBookNoteBizApplication.class)
 public class DruidTest {
-    @Resource
-    private ChannelService channelService;
-
-    @Test
-    public void channelTest(){
-        channelService.findAll().forEach(System.out::println);
-    }
-
 }
